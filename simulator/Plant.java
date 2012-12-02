97b5bf94394467e584c3b48f1eda2e1f341980db
Simeon Filipov <ssfilipov@gmail.com>
2012-12-02 23:22:34 +0000

    Update Plant.java
    I tried to start developing from the Plant class and found out it is not
    a good idea. It is better to start from the small independet classes. I
    commented my whole Plant code for future use.

--- simulator/Plant.java
+++ simulator/Plant.java
@@ -1,6 +1,42 @@
 package simulator;
 
-public class Plant {
-	//To be implemented
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+class Plant implements Serializable {
+//	/*
+//	 * More info about serialVersionUID:
+//	 * http://stackoverflow.com/questions/285793/what-is-a-serialversionuid-and-why-should-i-use-it
+//	 */
+//	private static final long serialVersionUID = 1L;
+//	/*
+//	 * All fields are declared as Objects (i.e. Integer) instead of primitives (i.e. int)
+//	 * This approach is more future-proof regarding serialisation
+//	 * Keep in mind that there is a performance trade-off for using Objects instead of primitives
+//	 */
+//	private String operatorName;
+//	private Integer timeStepsUsed;
+//	private Integer score;
+//	private List<Repair> beingRepaired;
+//	private Boolean isPaused;
+//	private List<Integer> highScores;
+//	private List<plantComponent> plantComponents;
+//	private List<plantComponent> failedComponents;
+//	
+//	/**
+//	 * This is the default constructor that is used 
+//	 * when there is no save game file present (i.e. new installation)
+//	 */
+//	public Plant(String operatorName) {
+//		this.operatorName = operatorName;
+//		this.timeStepsUsed = 0;
+//		this.score = 0;
+//		this.beingRepaired = new ArrayList<Repair>();
+//		this.isPaused = false;
+//		this.highScores = new ArrayList<Integer>(20);
+//		this.plantComponents = new ArrayList<plantComponent>();
+//		//Insert all plant components into plantComponents
+//		this.failedComponents = new ArrayList<plantComponent>();
+//	}
 }
