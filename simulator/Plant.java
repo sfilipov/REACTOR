a04d42616d1a4639db7af5a39cee9df55587c8b9
WillFrew <will.frew1@gmail.com>
2012-12-23 18:30:25 +0000

    Fix type declarations

--- simulator/Plant.java
+++ simulator/Plant.java
@@ -1,32 +1,33 @@
 package simulator;
 
 //import java.io.Serializable;
-//import java.util.ArrayList;
-//import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
-class Plant {
-//	private String operatorName;
-//	private int timeStepsUsed;
-//	private int score;
-//	private List<Repair> beingRepaired;
-//	private bool isPaused;
-//	private List<Integer> highScores;
-//	private List<plantComponent> plantComponents;
-//	private List<plantComponent> failedComponents;
-//	
-//	/**
-//	 * This is the default constructor that is used 
-//	 * when there is no saved game (i.e. new installation)
-//	 */
-//	public Plant(String operatorName) {
-//		this.operatorName = operatorName;
-//		this.timeStepsUsed = 0;
-//		this.score = 0;
-//		this.beingRepaired = new ArrayList<Repair>();
-//		this.isPaused = false;
-//		this.highScores = new ArrayList<Integer>(20);
-//		this.plantComponents = new ArrayList<plantComponent>();
-//		//Insert all plant components into plantComponents
-//		this.failedComponents = new ArrayList<plantComponent>();
-//	}
+public class Plant {
+	private String operatorName;
+	private int timeStepsUsed;
+	private int score;
+	private List<Repair> beingRepaired;
+	private boolean isPaused;
+	private List<Integer> highScores;
+	private List<PlantComponent> plantComponents;
+	private List<PlantComponent> failedComponents;
+	
+	/**
+	 * This is the default constructor that is used 
+	 * when there is no saved game (i.e. new installation)
+	 */
+	public Plant(String operatorName) {
+		this.operatorName = operatorName;
+		this.timeStepsUsed = 0;
+		this.score = 0;
+		this.beingRepaired = new ArrayList<Repair>();
+		this.isPaused = false;
+		this.highScores = new ArrayList<Integer>(20);
+		this.plantComponents = new ArrayList<PlantComponent>();
+		//Insert all plant components into plantComponents
+		this.failedComponents = new ArrayList<PlantComponent>();
+	}
+	
 }
