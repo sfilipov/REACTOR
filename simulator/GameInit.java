b5d5f13b768e4bb3a8f0ad85d29eb566e171c34d
WillFrew <will.frew1@gmail.com>
2012-12-30 19:14:29 +0000

    Add main method and initial class javadoc comment to GameInit

--- simulator/GameInit.java
+++ simulator/GameInit.java
@@ -1,5 +1,25 @@
 package simulator;
 
+/**
+ * GameInit class bootstraps the entire game.
+ * 
+ * It creates the Plant object and populates it
+ * with the system model defined in the JSON config file.
+ * It also instantiates the GUI and gives it a reference to 
+ * the presenter, which in turn is given a reference to the
+ * Plant (Model)
+ * 
+ * @author WillFrew
+ *
+ */
 public class GameInit {
+	
+	public GameInit() {
+		// Init Game! :D
+	}
+	
+	public static void main(String[] args) {
+		GameInit game = new GameInit();
+	}
 	
 }
