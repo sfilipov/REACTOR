9e64e15283ba8cc630fd36a1e4deeccdb9343458
WillFrew <will.frew1@gmail.com>
2012-12-31 02:14:12 +0000

    Update Valve class as per UML changes.

--- simulator/Valve.java
+++ simulator/Valve.java
@@ -1,100 +1,52 @@
 package simulator;
 
-class Valve extends PlantComponent {
+public class Valve extends PlantComponent {
 	private final static double VALVE_FAILURE_RATE = 0.0;
 	private final static int VALVE_REPAIR_RATE = 0;
-	private PlantComponentPair connectedTo; 
+	private final static boolean DEFAULT_OPEN_STATE = true; 
+	
 	private boolean open;
 	
 	/**
 	 * Creates a new valve that is connected to the specified
 	 * two plant components.
-	 * @param first the first plant component
-	 * @param second the second plant component
 	 */
-	Valve (PlantComponent first, PlantComponent second) {
+	public Valve () {
 		super(VALVE_FAILURE_RATE, VALVE_REPAIR_RATE);
-		this.connectedTo = new PlantComponentPair(first, second);
-		this.open = true;
+		this.open = DEFAULT_OPEN_STATE;
 	}
 	
 	/**
-	 * Creates a new valve that is connected to the specified
-	 * two plant components and sets it's position (open/closed).
-	 * @param first the first plant component
-	 * @param second the second plant component
+	 * Creates a new valve and sets it's position (open/closed)
 	 * @param open true if the valve is open
 	 */
-	Valve (PlantComponent first, PlantComponent second, boolean open) {
+	public Valve (boolean open) {
 		super(VALVE_FAILURE_RATE, VALVE_REPAIR_RATE);
-		this.connectedTo = new PlantComponentPair(first, second);
 		this.open = open;
-	}
-	
-	/**
-	 * Returns the first plant component that the valve is connected to.
-	 * @return the first plant component that the valve is connected to
-	 */
-	PlantComponent getFirstComponent() {
-		return connectedTo.getFirstComponent();
-	}
-
-	/**
-	 * Returns the second plant component that the valve is connected to.
-	 * @return the second plant component that the valve is connected to
-	 */	
-	PlantComponent getSecondComponent() {
-		return connectedTo.getSecondComponent();
 	}
 	
 	/**
 	 * Returns true if the valve is open.
 	 * @return true if the valve is open
 	 */
-	boolean isOpen() {
+	public boolean isOpen() {
 		return open;
 	}
 	
 	/**
-	 * Sets the position of the valve.
-	 * @param open true if the valve is open
+	 * Toggles the state of the valve.
 	 */
-	void setOpen(boolean open) {
-		this.open = open;
+	public void toggleState() {
+		open = !open;
 	}
 	
-	void updateState() {
-		//Insert implementation
+	public void updateState() {
+		// Just chill like a valve.
+		// (Valves do nothing themselves)
 	}
 	
-	void checkFailure() {
+	public void checkFailure() {
 		//Insert implementation 
 	}	
 	
-	/*
-	 * This class is used internally to represent the pair of components
-	 * that the particular valve is connected to. Once created, it only
-	 * provides methods to access the fields but not to change them.
-	 */
-	private final class PlantComponentPair {
-		/*
-		 * I assume a Valve doesn't change it's connections once it is created.
-		 * If you latter need to make the pair changeable then
-		 * create a setter.
-		 */
-		private PlantComponent first, second;
-		
-		PlantComponentPair(PlantComponent first, PlantComponent second) {
-			this.first = first;
-			this.second = second;
-		}
-		
-		PlantComponent getFirstComponent() {
-			return first;
-		}
-		
-		PlantComponent getSecondComponent() {
-			return second;
-		}
-	}
 }
