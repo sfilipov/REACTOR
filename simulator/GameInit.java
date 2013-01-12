275a069152ef7814f8a99e3a2bafbd767883aeb2
WillFrew <will.frew1@gmail.com>
2013-01-12 15:34:13 +0000

    Add basic instantiations to GameInit.

--- simulator/GameInit.java
+++ simulator/GameInit.java
@@ -6,16 +6,21 @@ package simulator;
  * It creates the Plant object and populates it
  * with the system model defined in the JSON config file.
  * It also instantiates the GUI and gives it a reference to 
- * the presenter, which in turn is given a reference to the
- * Plant (Model)
+ * the presenter. 
+ * The presenter is given a reference to the Plant (Model)
  * 
  * @author WillFrew
  *
  */
 public class GameInit {
 	
+	private Plant model;
+	private Interface view;
+	// private PlantPresenter presenter; 
+	
 	public GameInit() {
-		// Init Game! :D
+		model = new Plant("");
+		view = new Interface();
 	}
 	
 	public static void main(String[] args) {
