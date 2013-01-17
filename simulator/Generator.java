8198751e2d9eeee16aa82f97bce1aa098fbba0d6
Simeon Filipov <ssfilipov@gmail.com>
2013-01-17 18:56:19 +0000

    Add Generator

---
+++ simulator/Generator.java
@@ -0,0 +1,23 @@
+package simulator;
+
+public class Generator extends PlantComponent {
+	private static final int DIVISOR = 123; //Random number to make score look better (not a multiple of 10/100/1000)
+	
+	private int powerOutput;
+	private Turbine turbine;
+	
+	Generator(Turbine turbine) {
+		super(0.0, 0); //Perfect - never fails
+		this.turbine = turbine;
+		this.powerOutput = 0;
+	}
+	
+	@Override
+	public void updateState() {
+		powerOutput = turbine.getRpm() / DIVISOR;
+	}
+	
+	public int getPowerOutput() {
+		return powerOutput;
+	}
+}
