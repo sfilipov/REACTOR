47478ba146a714523c069582b970ef0ea2663803
Simeon Filipov <ssfilipov@gmail.com>
2013-01-19 15:08:06 +0000

    Update UIData, create 2 new exceptions

--- simulator/UIData.java
+++ simulator/UIData.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class UIData {
-	private PlantPresenter presenter;
+	private Plant plant;
 	
 	private String operatorName;
 	private int    score;
@@ -30,28 +30,112 @@ public class UIData {
 	private List<Pump>  pumps;
 	
 	
-	UIData(PlantPresenter presenter) {
-		this.operatorName = presenter.getOperatorName();
-		this.score        = presenter.getScore();
+	UIData(Plant plant) {
+		this.operatorName = plant.getOperatorName();
+		this.score        = plant.getScore();
 		
-		this.reactorHealth             = presenter.getReactorHealth();
-		this.reactorTemperature        = presenter.getReactorTemperature();
-		this.reactorMaxTemperature     = presenter.getReactorMaxTemperature();
-		this.reactorPressure           = presenter.getReactorPressure();
-		this.reactorMaxPressure        = presenter.getReactorMaxPressure();
-		this.reactorWaterVolume        = presenter.getReactorWaterVolume();
-		this.reactorMinSafeWaterVolume = presenter.getReactorMinSafeWaterVolume();
+		this.reactorHealth             = plant.getReactor().getHealth();
+		this.reactorTemperature        = plant.getReactor().getTemperature();
+		this.reactorMaxTemperature     = plant.getReactor().getMaxTemperature();
+		this.reactorPressure           = plant.getReactor().getPressure();
+		this.reactorMaxPressure        = plant.getReactor().getMaxPressure();
+		this.reactorWaterVolume        = plant.getReactor().getWaterVolume();
+		this.reactorMinSafeWaterVolume = plant.getReactor().getMinSafeWaterVolume();
 		
-		this.condenserHealth         = presenter.getCondenserHealth();
-	    this.condenserTemperature    = presenter.getCondenserTemperature();
-	    this.condenserMaxTemperature = presenter.getCondenserMaxTemperature();
-	    this.condenserPressure       = presenter.getCondenserPressure();
-	    this.condenserMaxPressure    = presenter.getCondenserMaxPressure();
-	    this.condenserWaterVolume    = presenter.getCondenserWaterVolume();
+		this.condenserHealth         = plant.getCondenser().getHealth();
+	    this.condenserTemperature    = plant.getCondenser().getTemperature();
+	    this.condenserMaxTemperature = plant.getCondenser().getMaxTemperature();
+	    this.condenserPressure       = plant.getCondenser().getPressure();
+	    this.condenserMaxPressure    = plant.getCondenser().getMaxPressure();
+	    this.condenserWaterVolume    = plant.getCondenser().getWaterVolume();
 	    
-	    this.controlRods = presenter.getControlRodsPercentage();
+	    this.controlRods = plant.getReactor().getPercentageLowered();
 	    
-	    this.valves = presenter.getValves();
-	    this.pumps  = presenter.getPumps();
+	    this.valves = plant.getValves();
+	    this.pumps  = plant.getPumps();
 	}
+	
+	public String getOperatorName() {
+		return operatorName;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public int getReactorHealth() {
+		return reactorHealth;
+	}
+	
+	public int getReactorTemperature() {
+		return reactorTemperature;
+	}
+	
+	public int getReactorMaxTemperature() {
+		return reactorMaxTemperature;
+	}
+	
+	public int getReactorPressure() {
+		return reactorPressure;
+	}
+	
+	public int getReactorMaxPressure() {
+		return reactorMaxPressure;
+	}
+	
+	public int getReactorWaterVolume() {
+		return reactorWaterVolume;
+	}
+	
+	public int getReactorMinSafeWaterVolume() {
+		return reactorMinSafeWaterVolume;
+	}
+	
+	
+	public int getCondenserHealth() {
+		return condenserHealth;
+	}
+	
+	public int getCondenserTemperature() {
+		return condenserTemperature;
+	}
+	
+	public int getCondenserMaxTemperature() {
+		return condenserMaxTemperature;
+	}
+	
+	public int getCondenserPressure() {
+		return condenserPressure;
+	}
+	
+	public int getCondenserMaxPressure() {
+		return condenserMaxPressure;
+	}
+	
+	public int getCondenserWaterVolume() {
+		return condenserWaterVolume;
+	}
+	
+	public int getControlRodsPercentage() {
+		return controlRods;
+	}
+	
+	public boolean isOpenValve(int valveID) throws ValveNotFoundException {
+		for (Valve v : valves) {
+			if (v.getID() == valveID) {
+				return v.isOpen();
+			}
+		}
+		throw new ValveNotFoundException();
+	}
+	
+	public boolean isOnPump(int pumpID) throws PumpNotFoundException {
+		for (Pump p : pumps) {
+			if (p.getID() == pumpID) {
+				return p.isOn();
+			}
+		}
+		throw new PumpNotFoundException();
+	}
+
 }
